e0de9600544ca113fd075626a9ed9f6e1ce700b0
beartropy <beartropy@gmail.com>
2025-10-28 20:42:27 -0300

    update card bg and padding

--- resources/views/presets/card.php
+++ resources/views/presets/card.php
@@ -4,21 +4,21 @@ return [
     'colors' => [
         'beartropy' => [
             'slot'      => 'text-gray-800 dark:text-gray-200',
-            'wrapper'   => 'bg-white dark:bg-gray-950/50 rounded-xl shadow-xl p-6 mt-4',
+            'wrapper'   => 'bg-white dark:bg-gray-900/30 rounded-xl shadow-xl p-3 mt-4',
             'title'     => 'text-lg font-semibold text-gray-800 dark:text-gray-200 border-b border-gray-300 dark:border-gray-700 pb-2 mb-4',
             'footer'    => 'pt-4 text-gray-800 dark:text-gray-200 border-t border-gray-300 dark:border-gray-700 mt-2 items-center justify-end flex space-x-2',
         ],
 
         'modal' => [
             'slot'      => 'text-gray-800 dark:text-gray-200',
-            'wrapper'   => 'bg-white dark:bg-gray-950/50 rounded-xl shadow-xl p-6',
+            'wrapper'   => 'bg-white dark:bg-gray-900/30 rounded-xl shadow-xl p-3',
             'title'     => 'text-lg font-semibold text-gray-800 dark:text-gray-200 border-b border-gray-300 dark:border-gray-700 pb-2 mb-4',
             'footer'    => 'pt-4 text-gray-800 dark:text-gray-200 border-t border-gray-300 dark:border-gray-700 mt-2 items-center justify-end flex space-x-2',
         ],
 
         'neutral' => [
             'slot'      => '',
-            'wrapper'   => 'bg-white dark:bg-gray-950/50 rounded-xl shadow-xl p-6',
+            'wrapper'   => 'bg-white dark:bg-gray-900/30 rounded-xl shadow-xl p-3',
             'title'     => '',
             'footer'    => '',
         ],
