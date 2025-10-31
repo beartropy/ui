930b02d23f792b61c2bc9810a691ec58a20b76ff
Beartropy <beartropy@gmail.com>
2025-10-30 22:42:19 -0300

    test update

--- resources/views/svg/beartropy-calendar.blade.php
+++ resources/views/svg/beartropy-calendar.blade.php
@@ -2,11 +2,4 @@
     <rect x="3" y="5" width="18" height="16" rx="3" class="fill-none" stroke-width="2"/>
     <path d="M8 3v4M16 3v4" stroke-width="1.5" stroke-linecap="round"/>
     <path d="M3 9h18" stroke-width="1.5" stroke-linecap="round"/>
-
-{{--     <circle cx="8" cy="13" r="1" />
-    <circle cx="12" cy="13" r="1" />
-    <circle cx="16" cy="13" r="1" />
-    <circle cx="8" cy="17" r="1" />
-    <circle cx="12" cy="17" r="1" />
-    <circle cx="16" cy="17" r="1" /> --}}
 </svg>
