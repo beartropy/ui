745e6592ad041ec55fdf759fdd32e5fa32668ed1
beartropy <beartropy@gmail.com>
2025-11-07 00:35:31 -0300

    update fab

--- resources/views/components/fab.blade.php
+++ resources/views/components/fab.blade.php
@@ -6,19 +6,27 @@
     $zIndex = $zIndex ?? 50;
     $right = $right ?? "1rem";
     $bottom = $bottom ?? "1rem";
+
+    $isLink = $attributes->has('href');
+
+    $tag = $isLink ? 'a' : 'button';
 @endphp
 
 <div class="fixed {{ $onlyMobile ? 'md:hidden' : '' }}" style="right: {{ $right }}; bottom: {{ $bottom }}; z-index: {{ $zIndex }};">
-  <button
-    type="button"
+  <{{ $tag }}
     {{ $attributes->merge([
       'class' => 'flex items-center justify-center  rounded-full shadow-lg transition '.$colorPreset['bg'] . ' ' . $colorPreset['text'] . ' ' . $colorPreset['bg_hover'] . ' ' . $sizePreset['fabButton']
     ]) }}
+    @if ($isLink)
+      href="{{ $attributes->get('href') }}"
+    @endif
+
+    {{ $attributes->except('href', 'class') }}
   >
     @if ($slot->isNotEmpty())
       {!! $slot !!}
     @else
         <x-beartropy-ui::icon name="{{$icon}}" class="{{$sizePreset['fabIcon']}}" />
     @endif
-  </button>
+  </{{ $tag }}>
 </div>
